dc36c5de63b79dfe93e3d3e408952318c1ac83cd
sazzadurrahman <sazz.rahm@gmail.com>
2022-11-28 16:58:49 -0800

    implemented linear search

---
+++ algorithms/linearSearch.py
@@ -0,0 +1,34 @@
+
+# Implement a linear search algorithm
+# O(N) time complexity
+
+import argparse
+
+def linear_search(arr:list, n:float)-> int:
+    """
+    arr: a list of numbers
+    n : the number to be searched
+    output : the index of the found item
+    """
+    i = 0
+    while i < len(arr):
+        if arr[i] == n:
+            return i
+        i+=1
+    return None
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--arr",nargs="+",type=float)
+    parser.add_argument("--n", type=float)
+    args = parser.parse_args()
+    print(linear_search(args.arr, args.n))
+
+
+
+    
+
+
+
