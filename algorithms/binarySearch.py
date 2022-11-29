d0db90ec2aaaf41c28ea215c2bbf0faac5f6d488
sazzadurrahman <sazz.rahm@gmail.com>
2022-11-28 17:40:37 -0800

    implemented binary search

---
+++ algorithms/binarySearch.py
@@ -0,0 +1,33 @@
+import argparse
+
+
+# the input array must be sorted
+# the recursive call ensures O(logN) time
+
+def binary_search(arr:list, n:float)-> int:
+    """
+    arr: a list of sorted numbers
+    n : the number to be searched
+    i : starting index
+    output : the index of the found item
+    """
+    l = len(arr)
+    h = l//2
+    if n>=arr[h]:
+        if n == arr[h]:
+            return h
+        else:
+            binary_search(arr[h+1:],n)
+    else:
+        return binary_search(arr[0:h],n)
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--arr",nargs="+",type=float)
+    parser.add_argument("--n", type=float)
+    args = parser.parse_args()
+    print(binary_search(args.arr, args.n))
