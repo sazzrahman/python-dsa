a6b9921e2cd0917aad0e4188cb60e367116e63dd
Sazz <sazz.rahm@gmail.com>
2022-12-01 12:00:30 -0800

    implemented get, unshift  and shift

--- data_structures/singlyLinkedList.py
+++ data_structures/singlyLinkedList.py
@@ -1,3 +1,9 @@
+
+# Singly Linked List
+# push, pop, insert, remove, get, shift, unshift
+
+
+
 import unittest
 
 
@@ -13,7 +19,7 @@ class SinglyLinkedList:
         self.tail = None
         self.length = 0
 
-    def push(self, val):
+    def push(self, val:int)->None:
         """
         pushes the value at the end of the current tail
         """
@@ -33,11 +39,72 @@ class SinglyLinkedList:
             # increment the length
             self.length += 1
 
-    def pop(self):
+
+    def shift(self) -> int:
+        """
+        returns the value of head and replaces the head with the next available node
+        
+        """
+        if self.head:
+            pop_val = self.head.val
+            if self.head.next:
+                self.head = self.head.next
+            else:
+                self.head = None
+            self.length-=1
+            return pop_val
+            
+        else:
+            return None
+
+
+
+    def unshift(self, val:int) -> Node:
+        """
+        inserts a node at the beginning of the list
+        returns the new Linked list
+        """
+        if self.head:
+            new_node = Node(val)
+            new_node.next = self.head
+            self.head = new_node
+            
+
+        else:
+            new_node = Node(val)
+            self.head = new_node
+            self.tail = new_node
+        
+        self.length +=1
+
+        return self
+
+
+        
+
+
+    def get(self,i:int) -> int:
+        """
+        returns the ith item's value in the Linked List
+        """
+
+        if i <0 or i>self.length-1:
+            return None
+        
+        n = 0
+        current_node = self.head
+        
+        while n <= i:
+            if n==i:
+                return current_node.val
+            current_node = current_node.next
+            n+=1
+           
+
+    def pop(self)->int:
         """
         pops the tail value of the list and assigns a new tail
         """
-
         if self.length == 0:
             return None
         current_node = self.head
@@ -67,27 +134,53 @@ class SinglyLinkedList:
 
 
 class TestLinkedList(unittest.TestCase):
-    def test_push(self):
+
+    
+    def populate_list(self):
         someList = SinglyLinkedList()
         someList.push(1)
         someList.push(2)
         someList.push(3)
         someList.push(9)
+        return someList
 
+
+    def test_push(self):
+        someList = self.populate_list()
+       
         self.assertEqual(someList.head.val, 1)
         self.assertEqual(someList.tail.val, 9)
 
     def test_pop(self):
-        someList = SinglyLinkedList()
-        someList.push(1)
-        someList.push(2)
-        someList.push(3)
-        someList.push(9)
-
+        someList = self.populate_list()
+       
         for i in range(someList.length):
             someList.pop()
 
         self.assertFalse(someList.pop())
+
+
+    def test_shift(self):
+        someList = self.populate_list()
+        self.assertEqual(someList.shift(),1)
+        self.assertEqual(someList.length,3)
+
+
+    def test_unshift(self):
+        someList = self.populate_list()
+        unshifted = someList.unshift(99)
+        self.assertEqual(unshifted.head.val,99)
+        self.assertEqual(type(unshifted),SinglyLinkedList)
+        self.assertEqual(unshifted.length,5)
+
+
+    def test_get(self):
+        someList = self.populate_list()
+
+        self.assertEqual(someList.get(0),1)
+        self.assertEqual(someList.get(3),9)
+        self.assertFalse(someList.get(11))
+        
 
 
 if __name__ == "__main__":
